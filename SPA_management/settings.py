f71c3aeda9d42c4ded3c6c81759d4344ddfc74ee
AdirthaBorgohain <adirthaborgohain@gmail.com>
2021-05-03 10:19:13 +0530

    debug mode turned on

--- SPA_management/settings.py
+++ SPA_management/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["PROD_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sarmah-brothers.herokuapp.com','127.0.0.1']
 
